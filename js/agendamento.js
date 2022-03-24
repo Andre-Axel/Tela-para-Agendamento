1d0202821caf10237348f511f28e814deaff79f6
Andre <andre.axel@outlook.com>
2022-03-24 08:59:59 -0300

    c2 - Tela de Agendamento Atualizada v2

--- js/agendamento.js
+++ js/agendamento.js
@@ -1,3 +1,35 @@
 function linkAgendamento(){
     window.open("https://simplybook.me/pt/");
 }
+
+function linkAgendamentoPsico(){
+    window.open("https://psicofpi.simplybook.me/");
+}
+function linkAgendamentoContab(){
+    window.open("https://admc.simplybook.me/");
+}
+function linkAgendaDireto(){
+    window.open("https://direitocoord.simplybook.me/");
+}
+function linkAgendaEnferma(){
+    window.open("https://enfpira.simplybook.me/");
+}
+
+function linkAgendaEngenha(){
+    window.open("https://brenocoordenador.simplybook.me/");
+}
+
+function numeroFisiNutriEdFis(){
+    alert("\n CONTATO PARA AGENDAMENTO:\n           (19) 2533-9064");
+}
+
+function numArq_Compu_RH_EngEle(){
+    alert("\n CONTATO PARA AGENDAMENTO: \n          (19) 98943-5785");
+}
+
+function numPedago_PP_CSTLogi(){
+    alert("\n CONTATO PARA AGENDAMENTO: \n           (19) 2533-9077");
+}
+
+
+
